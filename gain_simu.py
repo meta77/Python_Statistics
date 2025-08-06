1aa1c408b7b414001c75d5f23e4ab4572b16c436
Meta77 <horokusa@keio.jp>
2025-08-06 21:00:02 +0900

    パラメータの変更

--- gain_simu.py
+++ gain_simu.py
@@ -8,7 +8,7 @@ import scipy.stats as stats
 # パラメータの設定
 # ----------------------------
 initial_money = 50000            # 初期資金（円）
-win_rate = 0.82           # 勝率
+win_rate = 0.8           # 勝率
 gain = 0.04      # 勝ったときの利益　（%）
 loss = -0.05  # 負けたときの損失（%）
 num_trials = 70  # 取引回数（シミュレーション回数）
